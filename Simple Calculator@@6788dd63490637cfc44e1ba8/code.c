92b179348b0b6fbbba7d5fde37de44e5ff0ad0b3
Sumit Dhiman <143sumit05@gmail.com>
2025-02-17 22:31:39 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,33 +3,10 @@
 int main() {
     int a, b;
     char c;
+    scanf("%d %d %c",&a,&b,&c);
 
     // Input two integers and the operator
-    if (scanf("%d %d %c", &a, &b, &c) != 3) {
-        printf("error\n");
-        return 0;
-    }
-
-    // Check the operator using if-else statements
-    if (c == '+') {
-        printf("%d\n", a + b);
-    }
-    else if (c == '-') {
-        printf("%d\n", a - b);
-    }
-    else if (c == '*') {
-        printf("%d\n", a * b);
-    }
-    else if (c == '/') {
-        if (b == 0) {
-            printf("error\n");  // Handle division by zero
-        } else {
-            printf("%d\n", a / b);
-        }
-    }
-    else {
-        printf("error\n");  // Invalid operator case
-    }
-
+    if (c=='+')
+    printf("%d",a+b);
     return 0;
 }
